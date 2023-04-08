325f1a49db497c75cab9d046c6cee562096a9cc7
mohsenasadi501@yahoo.com <mohsenasadi501@yahoo.com>
2023-04-08 22:59:40 +0330

    use implementaion

---
+++ DataStructureAlgorithm/Part1/LinkedList.cs
@@ -0,0 +1,108 @@
+﻿namespace DataStructureAlgorithm.Part1
+{
+    internal class LinkedList
+    {
+        private class Node
+        {
+            public int value;
+            public Node next;
+            public Node(int value)
+            {
+                this.value = value;
+            }
+        }
+
+        private Node first;
+        private Node last;
+        public int size;
+
+        public void AddFirst(int item)
+        {
+            var mNode = new Node(item);
+            if (first == null)
+                last = first = mNode;
+            else
+            {
+                mNode.next = first;
+                first = mNode;
+            }
+            size++;
+        }
+        public void AddLast(int item)
+        {
+            var mNode = new Node(item);
+            if (first == null)
+                last = first = mNode;
+            else
+            {
+                last.next = mNode;
+                last = mNode;
+            }
+            size++;
+        }
+        public int IndexOf(int item)
+        {
+            int index = 0;
+            var current = first;
+            while (current != null)
+            {
+                if (current.value == item) return index;
+                current = current.next;
+                index++;
+            }
+            return -1;
+        }
+        public bool Contains(int value)
+        {
+            var current = first;
+            while (current != null)
+            {
+                if (current.value == value) return true;
+                current = current.next;
+            }
+            return false;
+        }
+        public void RemoveFirst()
+        {
+            if (first == null)
+                throw new InvalidOperationException();
+            if (first == last)
+            {
+                first = last = null;
+                size--;
+                return;
+            }
+            var second = first.next;
+            first.next = null;
+            first = second;
+            size--;
+        }
+        public void RemoveLast()
+        {
+            var current = first;
+            while (current != null)
+            {
+                if (current.next == last) break;
+                current = current.next;
+            }
+            last = current;
+            size--;
+        }
+        public int Size()
+        {
+            return size;
+        }
+        public int[] toArray()
+        {
+            int[] result = new int[size];
+            var current = first;
+            int index = 0;
+            while (current != null)
+            {
+                result[index++] = current.value;
+                current = current.next;
+            }
+            return result;
+        }
+    }
+}
--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataStructureAlgorithm.Part1;
+
+Console.WriteLine("*** LinkedList ***");
+LinkedList mLinkedList = new LinkedList();
+mLinkedList.AddLast(10);
+mLinkedList.AddLast(20);
+mLinkedList.AddLast(30);
+mLinkedList.AddLast(40);
+mLinkedList.AddLast(40);
+Console.WriteLine(mLinkedList.IndexOf(30));
+
+
+Console.WriteLine("*** Array ***");
+DataStructureAlgorithm.Part1.Array array = new DataStructureAlgorithm.Part1.Array(2);
+array.Add(10);
+array.Add(20);
+array.Add(30);
+array.Add(40);
+array.Add(50);
+array.Add(60);
+array.Add(70);
+array.Add(80);
+array.Add(90);
+array.Add(100);
+
+array.Remove(5);
+array.IndexOf(60);
+array.Print();
+
+Console.ReadKey();
