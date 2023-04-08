e64d4acb183053935a78ccf4ecab52ad88996be3
mohsenasadi501@yahoo.com <mohsenasadi501@yahoo.com>
2023-04-08 22:52:56 +0330

    Add big o sample with 'Runtime Complexity'

---
+++ DataStructureAlgorithm/Part1/Bigo.cs
@@ -0,0 +1,30 @@
+﻿namespace DataStructureAlgorithm.Part1
+{
+    internal class Bigo
+    {
+        public void log(int[] numbers)
+        {
+            //o(1)
+            Console.WriteLine(numbers[0]);
+        }
+        public void logLoop(int[] numbers, string[] texts)
+        {
+            //O(n) == O(n + m)
+
+            //o(n)
+            foreach (var item in numbers)
+                Console.WriteLine(item);
+            //o(m)
+            foreach (var item in texts)
+                Console.WriteLine(item);
+        }
+        public void LogNestedLoop(int[] numbers)
+        {
+            //o(n^3)
+            foreach (var item in numbers)
+                foreach (var item1 in numbers)
+                    foreach (var item2 in numbers)
+                        Console.WriteLine(item2);
+        }
+    }
+}
