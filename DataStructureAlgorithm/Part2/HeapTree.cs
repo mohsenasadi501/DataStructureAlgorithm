5779e99fe3adba429941d33c3c3ed26bbe1738f6
mohsenasadi501@yahoo.com <mohsenasadi501@yahoo.com>
2023-04-24 13:16:14 +0330

    remove implemetation

--- DataStructureAlgorithm/Part2/HeapTree.cs
+++ DataStructureAlgorithm/Part2/HeapTree.cs
@@ -33,6 +33,51 @@ namespace DataStructureAlgorithm.Part2
                 index = parent(index);
             }
         }
+        public void remove()
+        {
+            if (isEmpty())
+                throw new Exception();
+
+            items[0] = items[--size];
+            var index = 0;
+            while (index <= size && !isValidParent(index))
+            {
+                var largeChildIndex = largerChildIndex(index);
+                swap(index, largeChildIndex);
+                index = largeChildIndex;
+            }
+        }
+        public bool isEmpty()
+        {
+            return size == 0;
+        }
+        private int largerChildIndex(int index)
+        {
+            return (leftChild(index) > rightChild(index))
+                ? leftChildIndex(index)
+                : rightChildIndex(index);
+        }
+        private bool isValidParent(int index)
+        {
+            return items[index] >= leftChild(index) &&
+                items[index] >= rightChild(index);
+        }
+        private int leftChild(int index)
+        {
+            return items[leftChildIndex(index)];
+        }
+        private int rightChild(int index)
+        {
+            return items[rightChildIndex(index)];
+        }
+        private int leftChildIndex(int index)
+        {
+            return index * 2 + 1;
+        }
+        private int rightChildIndex(int index)
+        {
+            return index * 2 + 2;
+        }
         private int parent(int index)
         {
             return (index - 1) / 2;
